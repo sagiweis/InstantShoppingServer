196827d5cd53460f8a980d8836a2b987ce597cef
yasmin <yaskar0808@gmail.com>
2016-06-08 05:02:58 -0700

    קבצים חסרים

---
+++ InstantShoppingBL/CategoriesBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstantShoppingDataAccess;
+using InstantShoppingCommon;
+
+
+namespace InstantShoppingBL
+{
+    public class CategoriesBL
+    {
+        public static List<Category> GetCategories()
+        {
+               return CategoriesDataAccess.GetInstance().GetCategories();
+        }
+    }
+}
